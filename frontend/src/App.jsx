20866c4a821de00a1aa570407a43dd65e84d732b
Regis066 <mugisharegis72@gmail.com>
2023-06-15 08:35:40 +0200

    creation of bootstrap-navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import{Outlet} from 'react-router-dom'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Container className='my-2'>
+      <Outlet />
+    </Container>
+    </>
+  )
+}
+
+export default App
